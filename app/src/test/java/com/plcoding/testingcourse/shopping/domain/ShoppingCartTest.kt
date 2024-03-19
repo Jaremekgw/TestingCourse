de99115021307186f705fd5b7bf6e2bda38f8615
Jaroslaw Gawin <jaroslaw.gawin@mobica.com>
2024-03-19 13:34:44 +0100

    MSG: modify for repeated, param and private test
    Modify code to run repeated test functions,
    to run parameterized and how to run test
    for private functions
    
    Signed-off-by: Jaroslaw Gawin Mobica <jaroslaw.gawin@mobica.com>

--- app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/shopping/domain/ShoppingCartTest.kt
@@ -1,10 +1,15 @@
 package com.plcoding.testingcourse.shopping.domain
 
+import androidx.core.app.RemoteInput.Source
 import assertk.assertFailure
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 
 class ShoppingCartTest {
@@ -18,23 +23,34 @@ class ShoppingCartTest {
 
     // name: What that test doing,  What we expect
     // instead of:  fun addMultipleProducts_totalPriceSumCorrect()
-    @Test
-    fun `Add multiple products, total price sum is correct`() {
+    // @Test
+    @ParameterizedTest
+    @CsvSource(
+        "3,16.5",
+        "0,0.0",
+        "10,55.0",
+        "4,22.0",
+        "20,110.0"
+    )
+    fun `Add multiple products, total price sum is correct`(
+        quantity: Int,
+        expectedPrice: Double
+    ) {
         val product = Product (
             id = 0,
             name = "Ice cream",
             price = 5.5
         )
-        cart.addProduct(product = product, 4)
+        cart.addProduct(product = product, quantity)
 
         // ACTION
         val priceSum = cart.getTotalCost()
 
         // ASSERTION
-        assertThat(priceSum).isEqualTo(22.0)
+        assertThat(priceSum).isEqualTo(expectedPrice)
     }
 
-    @Test
+    @RepeatedTest(50)
     fun `Add product with negative quantity, throws Exception`() {
         val product = Product (
             id = 0,
@@ -45,7 +61,18 @@ class ShoppingCartTest {
         assertFailure {
             cart.addProduct(product = product, -1)
         }
+    }
 
+    @Test
+    fun `isValidProduct returns invalid for not existing product`() {
+        val product = Product (
+            id = 134,
+            name = "Ice cream",
+            price = 5.5
+        )
+
+        cart.addProduct(product = product, 4)
+        assertThat(cart.getTotalCost()).isEqualTo(0.0)
 
     }
 }
