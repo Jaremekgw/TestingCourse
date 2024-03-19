3d37b0a17926dfe0e7abd07bff05b18909557743
Jaroslaw Gawin <jaroslaw.gawin@mobica.com>
2024-03-19 11:44:31 +0100

    MSG: Add junit 5
    Add dependencies, plugin and useful library to use junit5
    For example run test 'addition_isCorrect()' and it fails
    
    Signed-off-by: Jaroslaw Gawin Mobica <jaroslaw.gawin@mobica.com>

--- app/src/test/java/com/plcoding/testingcourse/ExampleUnitTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/ExampleUnitTest.kt
@@ -1,7 +1,24 @@
 package com.plcoding.testingcourse
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+/**
+ * junit5 from this page:
+ * https://github.com/mannodermaus/android-junit5
+ *
+ */
+
+
+// only for juint4:
+// import org.junit.Assert.assertEquals
+// import org.junit.Test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +26,24 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    // This are called only once for all test
+    // @BeforeClass
+    // @AfterClass
+
+    @BeforeEach
+    fun setUp() {
+
+    }
+    @AfterEach
+    fun tearDown() {
+
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        // assertEquals(4, 2 + 2)
+        assertThat(2+3).isEqualTo(4)
+
     }
 }
